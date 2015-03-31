b98c6c9f23347dcd79d3560b87a51034af456f85
Hans <Hans@skolLaptop>
2015-03-31 12:52:04 +0200

    Ready for evaluation!

--- app/src/main/java/hansl/pfi3_2015_hans_l/MainActivity.java
+++ app/src/main/java/hansl/pfi3_2015_hans_l/MainActivity.java
@@ -2,6 +2,7 @@ package hansl.pfi3_2015_hans_l;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
--- assignment_1/src/main/java/hansl/assignment_1/MainActivity.java
+++ assignment_1/src/main/java/hansl/assignment_1/MainActivity.java
@@ -2,6 +2,7 @@ package hansl.assignment_1;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,19 +11,36 @@ import java.util.Random;
 
 public class MainActivity extends ActionBarActivity {
 
-    Random rand = new Random();
-    int n = 0;
+    //Used for writing a random quote from the list in strings.xml
+    //1 is for quotes, and 2 is for textView
+    Random rand1 = new Random();
+    Random rand2 = new Random();
+    int n1 = 0;
+    boolean n2 = false;
 
-
+    //When the button is pushed, a random quote from the list will be written to a random textView
     public void buttonOnClick(View v){
+        //Send to logcat
+        Log.i("MyAPP","Button Pressed");
+
         TextView t1 = (TextView) findViewById(R.id.text1);
         TextView t2 = (TextView) findViewById(R.id.text2);
 
+        //First, clear the textViews
+        t1.setText(null);
+        t2.setText(null);
+
         String[] quotes = getResources().getStringArray(R.array.quotes);
 
-        n = rand.nextInt(quotes.length);
+        n1 = rand1.nextInt(quotes.length);
+        n2 = rand2.nextBoolean();
 
-        t1.setText(quotes[n]);
+        if(n2 == true){
+            t1.setText(quotes[n1]);
+        }
+        if(n2 == false){
+            t2.setText(quotes[n1]);
+        }
     }
 
 
@@ -30,7 +48,38 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_portrait);
+        setContentView(R.layout.activity_main);
+        Log.i("onCreate","executed");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("onStart", "executed");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("onResume", "executed");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("onPause", "executed");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("onDestroy", "executed");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("onStop", "executed");
     }
 
     @Override
