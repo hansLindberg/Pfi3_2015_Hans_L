b087ab47560c3d3a373db727156af882c654cd22
Hans <Hans@skolLaptop>
2015-04-09 09:43:38 +0200

    First commit

---
+++ assignment_2/src/main/java/hansl/assignment_2/home.java
@@ -0,0 +1,29 @@
+package hansl.assignment_2;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class home extends Fragment{
+
+
+    public home() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle
+            savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_main,
+                container, false);
+        return view;
+    }
+
+
+}
